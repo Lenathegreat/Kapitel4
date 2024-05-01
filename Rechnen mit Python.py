c2d2ab3cf837f318c9e11a21bf3a4462137fbc2e
Lenathegreat <tandl.lena3@gmail.com>
2024-05-01 19:57:44 +0200

    if, if Ausdruck, Funktionen, Truthy, Falsey und User input

--- Rechnen mit Python.py
+++ Rechnen mit Python.py
@@ -1,5 +1,48 @@
 # Rechnen mit python
 
+## Truthy
+
+name = "" #Leerer Name
+
+print(name or "no name provided") ## Truthy wenn Name leer ist wird hier FALSE
+
+
+
+### IF
+user_input= ""
+def get_input():
+     user_input = input("How much do you want?")
+     return int(user_input)
+def check_int(number_to_add):
+
+    try:
+
+        int(number_to_add)
+        return int(number_to_add)
+    except ValueError:
+
+        print("this is not a number please try again:")
+        number_to_add=get_input()
+
+number_to_add = get_input()
+number_to_add= check_int(number_to_add)
+print(type(number_to_add))
+
+number = 10 + number_to_add
+
+if number > 11:
+    message = str(number) + " > 11"
+else:
+    message = "HAAAA"
+
+print(message)
+
+## if-ausdruck
+
+ifNumber = get_input()
+message_ifAusdruck = "10 or more" if ifNumber > 10 else "smaller"
+print(message_ifAusdruck)
+
 def result(operator):
     print(operator)
 
@@ -19,3 +62,6 @@ operator = 7 ** 3       # Potenz 7 hoch 3
 operator = 2 + (3 * ( 4 ** 2)) #innerste Klammer wird zuerst ausgeführt
 
 result(operator)
+
+
+
--- main.py
+++ main.py
@@ -14,6 +14,6 @@ def print_hi(name):
 if __name__ == '__main__':
     # you = input("who are you?")
     #print_hi(you)
-type("Lena")
+## type("Lena")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
